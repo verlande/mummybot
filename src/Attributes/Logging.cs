6daf2dc1cccaa9185610ac8e7fd774dc93ae83ee
mummy603 <realmummy@nigge.rs>
2019-06-18 13:35:48 +0100

    Disabled Logging Attribute

---
+++ src/Attributes/Logging.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+
+namespace mummybot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class Logging : PreconditionAttribute
+    {
+        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        {
+            if (!Services.GuildService.GuildMsgLogging[context.Guild.Id]) return Task.FromResult(PreconditionResult.FromSuccess());
+            return Task.FromResult(PreconditionResult.FromError("Command disabled as this guild has logging disabled"));
+        }
+    }
+}
