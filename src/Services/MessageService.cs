28372a95bba34dbcf866c388ca3252dbb6f1df65
mummy603 <realmummy@nigge.rs>
2020-03-11 22:11:31 +0000

    snipe fix

--- src/Services/MessageService.cs
+++ src/Services/MessageService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using NLog;
 
 namespace mummybot.Services
 {
@@ -17,6 +18,7 @@ namespace mummybot.Services
     public class MessageService
     {
         private readonly DiscordSocketClient _discord;
+        private readonly Logger _log;
         public static ConcurrentDictionary<ulong, Snipe> snipeDict = new ConcurrentDictionary<ulong, Snipe>();
 
         public MessageService(DiscordSocketClient discord)
@@ -27,14 +29,17 @@ namespace mummybot.Services
 
         private Task DeletedMessage(Cacheable<IMessage, ulong> cachedmsg, ISocketMessageChannel msg)
         {
-            Task.Run(async () =>
+            try
             {
-                var cachedmsgs = await cachedmsg.DownloadAsync();
-                if (cachedmsgs.Author.IsBot) return;
+                if (cachedmsg.Value.Author.IsBot) return Task.CompletedTask;
                 if (snipeDict.ContainsKey(cachedmsg.Value.Channel.Id))
-                    snipeDict.TryRemove(cachedmsg.Value.Channel.Id, out var _);
-                snipeDict.TryAdd(cachedmsg.Value.Channel.Id, new Snipe { AuthorId = cachedmsg.Value.Author.Id, Content = (cachedmsg.Value.Content == string.Empty) ? cachedmsg.Value.Attachments.First().Url : cachedmsg.Value.Content, CreatedAt = DateTime.UtcNow });
-            });
+                    snipeDict.TryRemove(cachedmsg.Value.Channel.Id, out _);
+                snipeDict.TryAdd(cachedmsg.Value.Channel.Id, new Snipe { AuthorId = cachedmsg.Value.Author.Id, Content = (cachedmsg.Value.Content == string.Empty) ? cachedmsg.Value.Attachments.First().Url : cachedmsg.Value.Content, CreatedAt = DateTime.UtcNow });            
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex);
+            }
             return Task.CompletedTask;
         }
     }
